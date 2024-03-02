552e77c49f13e39900fd543627f0023db7473a22
Ahmed Siddiqui <37452138+siddAhmed@users.noreply.github.com>
2024-03-02 07:28:39 +0000

    feat: add support for shadcn and aceternity components

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Dosis } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const dosis = Dosis({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dosis.className}>{children}</body>
     </html>
   );
 }
